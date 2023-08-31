96672c2bb8c3b76944db10c0ea2c88de03f0880a
twagirafelix <twagirafelix001@gmail.com>
2023-08-31 12:03:59 -0700

    updte

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,14 +5,15 @@
  * Return: 0 if big endian, 1 if little endian
  */
 
-int get_endianness(void)
+int get_endianness(void);
 {
-	                          unsigned int x = 1;
-	
-	           char *c;
 
-           	c = (char *)&x;
+unsigned int x = 1;
+
+char *c;
+
+c = (char *)&x;
 
 
-	return ((int)*c);
+return ((int)*c);
 }
