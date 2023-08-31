a0b3a66afae1cb17b343bf993efd770fa3618b25
twagirafelix <twagirafelix001@gmail.com>
2023-08-31 11:33:27 -0700

    manipulation bro

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,14 +11,15 @@ void print_binary(unsigned long int n)
 if (n >> 0)
 	{
 
-		if (n >> 1)
-	
-	print_binary(n >> 1);
+if (n >> 1)
 
-	_putchar((n & 1) + '0');
-	}
+print_binary(n >> 1);
+
+_putchar((n & 1) + '0');
+
+}
 	else
 	{
 		_putchar('0');
 	}
-}	
+}
